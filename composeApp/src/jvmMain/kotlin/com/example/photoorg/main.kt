749c8f9c96549f293424d885a93c6cf39464acee
MoruzAlexandru <moruz.alexandru@gmail.com>
2026-04-14 20:39:50 +0300

    feature/FE/PHOTORG-10 - add icons for window app

--- composeApp/src/jvmMain/kotlin/com/example/photoorg/main.kt
+++ composeApp/src/jvmMain/kotlin/com/example/photoorg/main.kt
@@ -2,6 +2,9 @@ package com.example.photoorg
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.compose.resources.painterResource
+import photoorg.composeapp.generated.resources.Res
+import photoorg.composeapp.generated.resources.photo_org_main_icon_color_photo
 import javax.swing.UIManager
 
 fun main() = application {
@@ -9,6 +12,7 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Photo Org",
+        icon = painterResource(Res.drawable.photo_org_main_icon_color_photo),
     ) {
         App()
     }
